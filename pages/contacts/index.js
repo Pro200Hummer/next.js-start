d207c59bbbf878b3016a625bc913c57caa94c34d
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2022-07-31 20:13:50 +0300

    06_lesson - add fetching data + getStaticProps

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,40 @@
 import {Heading} from "../../components/Heading";
 import Head from "next/head";
 
-const Contacts = () => {
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+
+    if(!data){
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            contacts: data
+        }
+    }
+}
+
+const Contacts = ({contacts}) => {
     return <div>
         <Head>
             <title>Contacts</title>
         </Head>
         <Heading text={'Contacts list:'}/>
-        <p>This is a users contacts</p>
+        <ul>
+            {
+                contacts && contacts.map(({id, name, email}) => {
+                    return <li key={id}>
+                        <p>id - {id}</p>
+                        <strong>name - {name}</strong>
+                        <p>email - {email}</p>
+                    </li>
+                })
+            }
+        </ul>
     </div>
 }
 
